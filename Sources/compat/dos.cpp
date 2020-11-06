c0019b8da0fe5737d0793dbd2d4cef365d64894c
d0p1 <contact@d0p1.eu>
2020-11-06 21:37:17 +0100

    compat: change 'enable' and 'disable' return type to void

--- Sources/compat/dos.cpp
+++ Sources/compat/dos.cpp
@@ -4,12 +4,12 @@
 
 static pthread_mutex_t mutex_stock = PTHREAD_MUTEX_INITIALIZER;
 
-int enable(void)
+void enable(void)
 {
     pthread_mutex_lock(&mutex_stock);
 }
 
-int disable(void)
+void disable(void)
 {
     pthread_mutex_unlock(&mutex_stock);
 }
